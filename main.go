6a575d49e37cdb11ceaf8536764452efb88df7d0
xiovv <xiovv@tutanota.com>
2019-04-22 20:30:58 +0200

    template commit

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(jsonData)
+}
+
+func getId(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
 	// Initialize router
 	r := mux.NewRouter()
 
 	// Route Handlers
+	r.HandleFunc("/api/endpoint", getData).Methods("GET")
+	r.HandleFunc("/api/endpoint/{id}", getId).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
